ff8e81f21ef7b968ca9733cdb580faf21013d7ec
CiprianRosu <cip.rosu@yahoo.com>
2022-06-10 17:12:41 +0200

    Add files via upload

--- src/router/AppRouter.js
+++ src/router/AppRouter.js
@@ -3,16 +3,33 @@ import { BrowserRouter, Switch, Route, Routes } from 'react-router-dom';
 import Header from '../components/Header';
 import AddBook from '../components/AddBook';
 import BooksList from '../components/BooksList';
+import useLocalStorage from '../hooks/useLocalStorage';
+import EditBook from '../components/EditBook';
+
 
 const AppRouter = () => {
+
+  const[books, setBooks] = useLocalStorage('books',[]);
+
   return (
     <BrowserRouter>
       <div>
         <Header />
         <div className="main-content">
           <Routes>
-            <Route element={<BooksList/>} path="/" exact={true} />
-            <Route element={<AddBook/>} path="/add" />
+            <Route 
+            element={<BooksList books={books} setBooks={setBooks}/>} 
+            path="/" 
+            exact={true} />
+           <Route
+            element={ <AddBook books={books} setBooks={setBooks} />}
+            path="/add"
+            />
+            <Route
+            element= {<EditBook books={books} setBooks={setBooks} />}
+             path="/edit/:id"
+            />
+            {/* <Route component={() => <Redirect to="/" />} /> */}
           </Routes>
         </div>
       </div>
